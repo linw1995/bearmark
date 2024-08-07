c0a1d7c4fd378c7f6597615c77c002710a48a928
林玮 (Jade Lin) <linw1995@icloud.com>
2024-08-07 20:39:58 +0800

    Normalize paths before searching

--- src/db/search.rs
+++ src/db/search.rs
@@ -43,7 +43,7 @@ type BookmarkFilter = Box<
 fn join_folder_path(cwd: &str, p: &str) -> String {
     const PATH_SEP: char = '/';
     if p.starts_with(PATH_SEP) {
-        p.to_string()
+        p.trim_end_matches(PATH_SEP).to_string()
     } else {
         cwd.trim_end_matches(PATH_SEP)
             .split(PATH_SEP)
