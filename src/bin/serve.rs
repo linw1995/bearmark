63c20cd378444bfdfc44465e3950eba281fdf03a
林玮 (Jade Lin) <linw1995@icloud.com>
2024-08-15 13:31:51 +0800

    Serve with static files (#8)
    * Serve with static files
    
    * Downloading artifacts requires token
    
    * Downloading artifacts requires right run-id
    
    * Downloading artifacts requires right run-id
    
    * Fix wrong value
    
    * Make CD build reproductable
    
    * Update actions version

--- src/bin/serve.rs
+++ src/bin/serve.rs
@@ -9,12 +9,15 @@ use rocket_db_pools::Database;
 #[launch]
 #[cfg(not(tarpaulin_include))]
 async fn rocket() -> _ {
+    use rocket::fs::FileServer;
+
     bearmark::utils::logging::setup_console_log();
     bearmark::db::connection::run_migrations().await;
 
     rocket::build()
+        .mount("/", FileServer::from("./static"))
         .attach(Db::init())
-        .mount("/bookmarks", bookmark::routes())
-        .mount("/tags", tag::routes())
-        .mount("/folders", folder::routes())
+        .mount("/api/bookmarks", bookmark::routes())
+        .mount("/api/tags", tag::routes())
+        .mount("/api/folders", folder::routes())
 }
