5d127ff4127600a5eaeea6c8c73f586e29e9d887
林玮 (Jade Lin) <linw1995@icloud.com>
2024-09-27 19:53:30 +0800

    Customize API configs provider

--- bearmark-api/src/bin/serve.rs
+++ bearmark-api/src/bin/serve.rs
@@ -5,8 +5,7 @@ use bearmark_api::api::configs::Config;
 use bearmark_api::api::fairings::db::Db;
 use bearmark_api::api::{bookmark, folder, tag};
 
-use rocket::fairing::AdHoc;
-use rocket::figment::providers::Serialized;
+use rocket::figment::providers::{Env, Serialized};
 use rocket::fs::FileServer;
 use rocket_db_pools::Database;
 
@@ -16,10 +15,15 @@ async fn rocket() -> _ {
     bearmark_api::utils::logging::setup_console_log();
     bearmark_api::db::connection::run_migrations().await;
 
-    let config = rocket::Config::figment().merge(Serialized::defaults(Config::default()));
-    let ui_path = config.extract_inner::<Option<String>>("ui_path").unwrap();
+    let cfg_provider = rocket::figment::Figment::from(rocket::Config::default())
+        .merge(Env::prefixed("BM_").global())
+        .merge(Serialized::defaults(Config::default()));
 
-    let mut builder = rocket::build();
+    let ui_path = cfg_provider
+        .extract_inner::<Option<String>>("ui_path")
+        .unwrap();
+
+    let mut builder = rocket::custom(cfg_provider);
     if let Some(ui_path) = ui_path {
         // Serve the UI files if the path is provided
         builder = builder.mount("/", FileServer::from(ui_path));
@@ -29,5 +33,4 @@ async fn rocket() -> _ {
         .mount("/api/bookmarks", bookmark::routes())
         .mount("/api/tags", tag::routes())
         .mount("/api/folders", folder::routes())
-        .attach(AdHoc::config::<Config>())
 }
