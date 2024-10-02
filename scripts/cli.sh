82faf180d0f8546879485ddb4c29dc80aa51d4df
林玮 (Jade Lin) <linw1995@icloud.com>
2024-10-02 23:23:04 +0800

    Fix Dynamic linking error on MacOS

--- scripts/cli.sh
+++ scripts/cli.sh
@@ -32,8 +32,11 @@ cleanup_profraw_files() {
 
 cd "$(dirname "$0")"/../
 
-tarpaulin_args="--workspace --exclude bearmark-macro --include-tests --skip-clean --out html -- --show-output --test-threads 1"
-tarpaulin_xml_args="--workspace --exclude bearmark-macro --include-tests --skip-clean --out xml -- --show-output --test-threads 1"
+tarpaulin_args="--workspace --include-tests --skip-clean --out html -- --show-output --test-threads 1"
+tarpaulin_xml_args="--workspace --include-tests --skip-clean --out xml -- --show-output --test-threads 1"
+
+export RUST_SRC_PATH="$(rustc --print sysroot)/lib/rustlib/src/rust/src"
+export DYLD_LIBRARY_PATH="$(rustc --print sysroot)/lib:$DYLD_LIBRARY_PATH"
 
 main() {
 	action=${1-}
