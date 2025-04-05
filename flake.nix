0b37a91b0340c3633feabac16dbbf4509d0b5170
林玮 (Jade Lin) <linw1995@icloud.com>
2025-04-05 11:02:26 +0800

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -1,53 +1,67 @@
 {
-  description = "Development Setup";
-
   inputs = {
     utils.url = "github:numtide/flake-utils";
-    stable.url = "github:NixOS/nixpkgs/nixos-24.05";
-    unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
+    nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
   };
 
   outputs = {
-    stable,
-    unstable,
+    nixpkgs,
+    nixpkgs-unstable,
     utils,
     ...
   }:
     utils.lib.eachDefaultSystem
     (
       system: let
-        pkgs = import stable {inherit system;};
+        pkgs = import nixpkgs {inherit system;};
+        unstable-pkgs = import nixpkgs-unstable {inherit system;};
         lib = pkgs.lib;
-        unstable-pkgs = import unstable {inherit system;};
-        cross = import stable {
-          inherit system;
-          crossSystem = {config = "x86_64-unknown-linux-musl";};
-        };
+        packages = with pkgs; [
+          pre-commit
+        ];
+        nativeBuildInputs = with pkgs; [
+          pkg-config
+          gcc # proc macro 等编译过程，需要原架构 toolchians
+        ];
+        buildInputs =
+          (with pkgs; [
+            openssl.dev
+            libiconv
+          ])
+          ++ (with unstable-pkgs; [
+            postgresql.dev
+          ]);
       in {
-        # Used by `nix develop`
-        devShells.default = pkgs.mkShell {
-          nativeBuildInputs = with pkgs; [
-            pkg-config
-          ];
-          buildInputs =
-            [
-              pkgs.openssl.dev
-              pkgs.libiconv
-              unstable-pkgs.postgresql.dev
-            ]
-            ++ lib.optionals pkgs.stdenv.isDarwin [
-              pkgs.darwin.apple_sdk.frameworks.SystemConfiguration
-            ];
-          packages = with pkgs; [
-            pre-commit
-          ];
-        };
-        devShells.x86_64-unknown-linux-musl = cross.mkShell (with cross.pkgsMusl; {
-          nativeBuildInputs = [
-            pkg-config
-          ];
-          CARGO_TARGET_X86_64_UNKNOWN_LINUX_MUSL_LINKER = "${stdenv.cc.targetPrefix}cc";
-        });
+        devShells =
+          {
+            default = pkgs.mkShell {
+              inherit nativeBuildInputs;
+              inherit buildInputs;
+              inherit packages;
+            };
+          }
+          // builtins.listToAttrs (map (target: {
+            name = target;
+            value = let
+              cross = import nixpkgs {
+                inherit system;
+                crossSystem = {config = target;};
+              };
+              cpkgs = cross.pkgsMusl;
+            in (cross.mkShell {
+              inherit nativeBuildInputs;
+              inherit buildInputs;
+              inherit packages;
+              env = let
+                normalized = lib.strings.toUpper (builtins.replaceStrings ["-"] ["_"] target);
+              in
+                with cpkgs; {
+                  # https://doc.rust-lang.org/cargo/reference/environment-variables.html#configuration-environment-variables
+                  "CARGO_TARGET_${normalized}_LINKER" = "${stdenv.cc.targetPrefix}cc";
+                };
+            });
+          }) ["x86_64-unknown-linux-musl" "aarch64-unknown-linux-musl"]);
       }
     );
 }
